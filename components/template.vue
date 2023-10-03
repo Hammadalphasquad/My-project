afac50ab88a2215bef53d97d8f184839850b8ae1
Hammadalphasquad <hammad.alphasquad@gmail.com>
2023-10-03 21:46:30 +0500

    first commit

---
+++ components/template.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- Top-bar -->
+    <div class="pl-10 py-6 border-[#1A202C] border-b-2">
+      <button
+        class=" text-base font-bold text-white cursor-pointer px-10 py-3 rounded-tl-[11px] rounded-tr-[20px] rounded-br-[6px] rounded-bl-[33px] bg-[#2D3648] "
+      >
+        Logo
+      </button>
+    </div>
+    <!-- Side-bar -->
+    <div class="flex">
+    <div class="max-w-[260px] w-full h-[884px] text-[#717D96] bg-[#EDF0F7] border-[#1A202C] border-r-2 py-6">
+      <div class="flex items-center py-3 pl-10 gap-2.5">
+        <img src="/images/icon-placeholder.png" alt="" />
+        <p class="text-sm leading-6 font-medium">Products</p>
+      </div>
+      <div class="flex items-center py-3 hover:bg-[#E2E7F0] pl-10 gap-[10px] hover:text-[#2D3648]">
+        <img src="/images/icon-placeholder.png" alt="" />
+        <p class="text-sm leading-6 font-medium  ">Settings</p>
+      </div>
+      <div class="">
+        <p class="hover:bg-[#D4D8DF] py-2 leading-6 text-sm font-medium pl-[67px]">Categories</p>
+        <p class="hover:bg-[#D4D8DF] py-2 leading-6 text-sm font-medium pl-[67px]">Product Type</p>
+        <p class="hover:bg-[#D4D8DF] py-2 leading-6 text-sm font-medium pl-[67px]">Product Size</p>
+        <p class="hover:bg-[#D4D8DF] py-2 leading-6 text-sm font-medium pl-[67px]">Attributes</p>
+      </div>
+    </div>
+    <!-- Heading -->
+    <div class="w-full">
+<p class="text-[32px] font-bold leading-[41.6px] border-b-2 py-8 pl-[67px] border-[#1A202C] text-[#2D3648]">
+    Categories
+</p>
+    </div>
+</div>
+  </div>
+</template>
