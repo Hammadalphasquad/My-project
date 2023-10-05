36da89b57ce1dfaa2c77db3ae18b4033b8c44948
Hammadalphasquad <hammad.alphasquad@gmail.com>
2023-10-05 22:02:44 +0500

    updated

--- components/template.vue
+++ components/template.vue
@@ -8,9 +8,8 @@
         Logo
       </button>
     </div>
-
-    <!-- Side-bar -->
     <div class="flex">
+      <!-- Side-bar -->
       <div
         class="max-w-[260px] w-full h-[833px] text-[#717D96] bg-[#EDF0F7] border-[#1A202C] border-r-2 py-6"
       >
@@ -62,47 +61,77 @@
 
         <!-- Content -->
         <div class="flex items-end gap-4">
-        <div class="pl-[67px] pt-[37px] max-w-[452px] w-full">
-          <p class="text-base font-medium text-gray-800 leading-4 mb-6">
-            Categories
-          </p>
-          <div class="w-full flex flex-col gap-8">
-            <input
-              type="text"
-              placeholder="Print Only"
-              class="text-sm font-normal max-h-[46px] leading-[14px] w-full border rounded-[4px] py-4 pl-4 border-gray-300 hover:border-gray-400"
+    <div class="pl-[67px] pt-[37px] max-w-[452px] w-full">
+      <p class="text-base font-medium text-gray-800 leading-4 mb-6">
+        Categories
+      </p>
+      <!-- Inputs -->
+      <div class="w-full flex flex-col gap-8">
+        <!-- Existing input -->
+        <div class="flex items-center gap-3" v-for="(input, index) in inputs" :key="index">
+          <input
+            type="text"
+            v-model="input.value"
+            :placeholder="'Input ' + (index + 1)"
+            class="text-sm font-normal max-h-[46px] leading-[14px] w-full border rounded-[4px] py-4 pl-4 border-gray-300 hover:border-gray-400"
+          />
+          <!-- Cross Icon -->
+          <svg
+            v-if="index > 0"
+            class="cursor-pointer"
+            width="37"
+            height="37"
+            viewBox="0 0 37 37"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            @click="removeInput(index)"
+          >
+            <circle cx="18.5" cy="18.5" r="18.5" fill="#F0F0F0" />
+            <path
+              d="M24.25 22.25L13.75 11.75"
+              stroke="#1E293B"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             />
-            <div class="flex items-center gap-[13px]">
-              <input
-                type="text"
-                placeholder="Print and Mail"
-                class="text-sm font-normal max-h-[46px] leading-[14px] w-full border rounded-[4px] py-4 pl-4 border-gray-300 hover:border-gray-400"
-              />
-            
-            </div>
-            <input
-              type="text"
-              placeholder="Electronic"
-              class="text-sm font-normal max-h-[46px] leading-[14px] w-full border rounded-[4px] py-4 pl-4 border-gray-300 hover:border-gray-400"
+            <path
+              d="M24.25 11.75L13.75 22.25"
+              stroke="#1E293B"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             />
-          </div>
+          </svg>
         </div>
-        <div class="flex flex-col gap-10 mb-1">
-          <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="18.5" cy="18.5" r="18.5" fill="#F0F0F0"/>
-<path d="M24.25 22.25L13.75 11.75" stroke="#1E293B" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M24.25 11.75L13.75 22.25" stroke="#1E293B" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-<svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="18.5" cy="18.5" r="18.5" fill="#F0F0F0"/>
-<path d="M24.25 22.25L13.75 11.75" stroke="#1E293B" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M24.25 11.75L13.75 22.25" stroke="#1E293B" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-
+        <!-- Add Button -->
+        <div
+          class="flex justify-center py-[2px] w-full bg-[#F0F0F0] rounded-[4px] cursor-pointer hover:bg-gray-200"
+          @click="addInput"
+        >
+          <svg
+            width="34"
+            height="34"
+            viewBox="0 0 34 34"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.9705 24.3951V9.5459"
+              stroke="#1E293B"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M24.3951 16.9705H9.54584"
+              stroke="#1E293B"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </div>
       </div>
+    </div>
 
-
+    <!-- Cross icons -->
+  </div>
       </div>
     </div>
   </div>
@@ -112,5 +141,20 @@
 import { ref } from "vue";
 
 const isSettingsOpen = ref(false);
-console.log(isSettingsOpen.value, "ksf");
+
+
+const inputs = ref([
+  { value: 'Print Only' },
+  { value: 'Print and Mail' },
+  { value: 'Electronic' },
+]);
+
+const addInput = () => {
+  inputs.value.push({ value: '' });
+};
+
+const removeInput = (index) => {
+  inputs.value.splice(index, 1);
+};
+
 </script>
